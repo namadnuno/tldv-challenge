2db380270c6b3e4a173b65416596a70b35e2ecee
Nuno Alexandre <nunnomalex@gmail.com>
2022-05-22 00:21:43 +0100

    add rejectUnauthorized option to db connection

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -7,7 +7,10 @@ module.exports = ({ env }) => ({
           port: env.int("DATABASE_PORT", 5432),
           database: env("DATABASE_NAME", "strapi"),
           user: env("DATABASE_USERNAME", "strapi"),
-          password: env("DATABASE_PASSWORD", "strapi")
+          password: env("DATABASE_PASSWORD", "strapi"),
+          ssl: {
+            rejectUnauthorized: false
+          },
       },
   debug: false,
   },

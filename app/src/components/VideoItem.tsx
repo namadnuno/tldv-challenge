2dc7995bd877460d201888f2259b743c500cec2e
Nuno Alexandre <nunnomalex@gmail.com>
2022-05-21 23:22:17 +0100

    improve list videos page styling

--- app/src/components/VideoItem.tsx
+++ app/src/components/VideoItem.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { Heading, Link } from '@chakra-ui/react';
+import {
+  Heading, Link, Image, Flex, Box, Badge,
+} from '@chakra-ui/react';
 import { Link as ReachLink } from 'react-router-dom';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import { Video } from '../api/types';
+
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 type Props = {
     video: Video
 }
 
-const VideoItem: React.FC<Props> = ({ video }) => <Link as={ReachLink} to={`/${video.id}`}><Heading>{video.attributes.title}</Heading></Link>;
+const formatToHumans = (date: string) => dayjs().to(date);
+
+const VideoItem: React.FC<Props> = ({ video }) => (
+  <Flex shadow="base" borderRadius="base" gap={4}>
+    <Image
+      boxSize="150px"
+      objectFit="cover"
+      src={`https://img.youtube.com/vi/${video.attributes.slug}/0.jpg`}
+      alt={video.attributes.title}
+    />
+    <Box padding={4}>
+      <Link as={ReachLink} to={`/${video.id}`}><Heading fontSize="md">{video.attributes.title}</Heading></Link>
+      {
+          video.attributes.publishedAt
+      && <Badge>{formatToHumans(video.attributes.publishedAt)}</Badge>
+      }
+    </Box>
+  </Flex>
+);
 
 export default VideoItem;
--- app/src/pages/VideosList.tsx
+++ app/src/pages/VideosList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Alert, AlertIcon, Box, Skeleton, Stack,
+  Alert, AlertIcon, Box, Grid, Skeleton, Stack,
 } from '@chakra-ui/react';
 import VideoItem from '../components/VideoItem';
 import useVideos from '../queries/useVideos';
@@ -28,11 +28,11 @@ function VideosList() {
 
   return (
     <Box>
-      <Stack gap={6}>
+      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
         {
                     data?.data.map((video) => (<VideoItem key={video.id} video={video} />))
                 }
-      </Stack>
+      </Grid>
     </Box>
   );
 }

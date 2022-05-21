d1b50196649d23ea0d76c96746a0b6747637de61
Nuno Alexandre <nunnomalex@gmail.com>
2022-05-22 00:45:40 +0100

    get api url through env var

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:1337/api';
+const baseURL = process.env.REACT_APP_API_BASE_URL ? process.env.REACT_APP_API_BASE_URL : 'http://localhost:1337/api';
 
 axios.defaults.adapter = require('axios/lib/adapters/http');
 
